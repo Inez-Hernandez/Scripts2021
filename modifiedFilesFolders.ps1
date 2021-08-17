ee63bbf87c587c3186f302b3e729bc322523abec
Inez-Hernandez <ihernandez@tpgrp.com>
2021-08-17 09:13:02 -0700

    Modified date script added

---
+++ modifiedFilesFolders.ps1
@@ -0,0 +1,28 @@
+# Author: Inez Hernandez Fuerte   Date: 8/17/2021
+# Description: Gets a report of any files in the user inputed folders and their subfolder
+# that have been newly created or modified since the user inputted date ticket: https://tpgrp.teamwork.com/desk/tickets/5579297
+# Source: Conver user input to date: https://community.idera.com/database-tools/powershell/powertips/b/tips/posts/converting-user-input-to-date
+# Itterate through folder: https://community.spiceworks.com/topic/2143488-powershell-script-to-list-folders-that-have-files-with-date-modified-in-a-range
+
+# $a = NULL
+# $a = Get-ChildItem \\server\lll\Received_Orders\*.* | Where{$_.LastWriteTime -ge (Get-Date).AddDays(-7)}
+# if ($a = (Get-ChildItem \\server\llll\Received_Orders\*.* | Where{$_.LastWriteTime -ge (Get-Date).AddDays(-7)}))
+# {
+# }
+# Else
+# {
+#   'STORE lll HAS NOT RECEIVED ANY ORDERS IN THE PAST 7 DAYS'
+# }
+
+# using -as flag to check valid user input and then conver to DateTime
+$date = Read-Host 'Enter modified since date:'
+if (($date -as [DateTime]) -ne $null) {
+  $date = [DateTime]::Parse($date)
+} else {
+  'Error in date, format as: MM/DD/YYYY'
+}
+
+# now that we have the user inputs, return the file(s)
+Get-ChildItem C:\Users\ihernandez\Desktop -File -Recurse |
+    Where-Object { $_.LastWriteTime -gt $date } |
+    select-Object FullName, LastWriteTime
